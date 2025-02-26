c1672a256a6ef7206e8825d10f347f54f812d1b0
Marcelo <marcelo.arraes@gmail.com>
2025-02-26 19:28:27 +1030

    feat: made login page

--- app/components/LoginScreen.tsx
+++ app/components/LoginScreen.tsx
@@ -1,13 +1,13 @@
 import { Button, Modal, TitleBar, List } from "@react95/core";
-import { Computer, Keys } from "@react95/icons";
+import { Computer, Keys, msrating105Data, Msrating105 } from "@react95/icons";
 import { useState } from "react";
 
 export function LoginScreen({ onLogin }: { onLogin: () => void }) {
   const [showModal, toggleShowModal] = useState(true);
-  const handleOpenModal = () => toggleShowModal(true);
+  // const handleOpenModal = () => toggleShowModal(true);
   const handleCloseModal = () => toggleShowModal(false);
-  const handleButtonClick = (e: React.MouseEvent<HTMLLIElement>) =>
-    alert(e.currentTarget.value);
+  // const handleButtonClick = (e: React.MouseEvent<HTMLLIElement>) =>
+  //   alert(e.currentTarget.value);
   return (
     <div className="flex h-full w-full items-center justify-center bg-[#008080]">
       <Modal
@@ -28,46 +28,64 @@ export function LoginScreen({ onLogin }: { onLogin: () => void }) {
           />,
           <TitleBar.Close key="close" onClick={handleCloseModal} />,
         ]}
-        buttons={[
-          {
-            value: "Ok",
-            onClick: handleButtonClick,
-          },
-          {
-            value: "Cancel",
-            onClick: handleButtonClick,
-          },
-        ]}
-        // menu={[
-        //   {
-        //     name: "File",
-        //     list: (
-        //       <List width="200px">
-        //         <List.Item onClick={handleCloseModal}>Exit</List.Item>
-        //       </List>
-        //     ),
-        //   },
-        //   {
-        //     name: "Edit",
-        //     list: (
-        //       <List width="200px">
-        //         <List.Item>Copy</List.Item>
-        //       </List>
-        //     ),
-        //   },
-        // ]}
       >
-        <Modal.Content
-          width="300px"
-          height="160px"
-          boxShadow="$in"
-          // bgColor="white"
-        >
-          <Keys variant="16x16_4"/>
-          Simple modal
-        </Modal.Content>
+        <div className="mb-4 flex flex-col items-center mr-6 ml-6 justify-between text-xl">
+          <div className="flex flex-row items-center justify-between">
+            <Msrating105 variant="32x32_4" className="mr-6 h-14 w-14" />
+            <div>
+              <div className="mt-4">
+                <span className="m-6 p-6 text-xl">
+                  Type a user name and password to log on to Windows
+                </span>
+              </div>
+              <div className="flex flex-col">
+                <label
+                  className="mb-1 block text-sm font-bold"
+                  htmlFor="username"
+                >
+                  Username:
+                </label>
+                <input
+                  type="text"
+                  id="username"
+                  className="w-full rounded border bg-gray-300 px-3 py-2"
+                  value="Administrator"
+                  disabled
+                />
+              </div>
+              <label
+                className="mb-1 block text-sm font-bold"
+                htmlFor="password"
+              >
+                Password:
+              </label>
+              <input
+                type="password"
+                id="password"
+                className="w-full rounded border bg-gray-300 px-3 py-2"
+                value="********"
+                disabled
+              />
+            </div>
+            <div className="flex flex-col gap-3 ml-5">
+              <Button
+                // className="w-full bg-[#0000a0] text-white hover:bg-[#000080]"
+                onClick={onLogin}
+              >
+                <span className="text-xl">Login</span>
+              </Button>
+              <Button
+                // className="w-full bg-[#0000a0] text-white hover:bg-[#000080]"
+                onClick={handleCloseModal}
+              >
+                <span className="text-xl">Cancel</span>
+              </Button>
+            </div>
+          </div>
+          <div className="mb-4"></div>
+        </div>
       </Modal>
-      <div className="w-96 rounded-md bg-[#c0c0c0] p-4 shadow-md">
+      {/* <div className="w-96 rounded-md bg-[#c0c0c0] p-4 shadow-md">
         <h1 className="mb-4 text-center text-2xl font-bold">Windows 95</h1>
         <div className="mb-4">
           <label className="mb-1 block text-sm font-bold" htmlFor="username">
@@ -99,7 +117,7 @@ export function LoginScreen({ onLogin }: { onLogin: () => void }) {
         >
           Login
         </Button>
-      </div>
+      </div> */}
     </div>
   );
 }
